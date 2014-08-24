c8b3d6ce5165917857c0fa25ae48701862932ff4
enkhalifapro <enkhalifapro@gmail.com>
2014-08-24 15:54:40 -0700

    init possible mail by rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Possiblemail::Application.config.secret_token = 'e18f6514279af67608602e19d594e08b5dbadef56b269be13c4d5bc9d6cb3edbc6d3338e1fc55430426d3a9615b5cf491d4dd4978d516e5b41d6083dbcdc6aa1'
